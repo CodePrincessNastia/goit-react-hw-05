3e9e280f0a9d0e556cf5635626347b5c2e06b30a
CodePrincessNastia <145288463+CodePrincessNastia@users.noreply.github.com>
2024-05-09 20:51:31 +0300

    mcast

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,4 +1,4 @@
-import css from './MovieCast.module.css';
+import css from './Moviecast.module.css';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieCast } from '../../movies-api';
